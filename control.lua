d72c9e10b4a46aeb550fefe1ace7ee7e5f2154a2
Bozerg <jnging2@gmail.com>
2024-11-12 00:26:34 -0600

    update for Factorio 2.0, stop trying to do biter things with Py

--- control.lua
+++ control.lua
@@ -1,8 +1,7 @@
 local util = require("util")
 local crash_site = require("crash-site")
 
-itemsToAdd = {["transport-belt"] = 100,  
-				["firearm-magazine"] = 100}
+itemsToAdd = {["transport-belt"] = 100}
 
 script.on_event(defines.events.on_player_created, function(event)
     local player = game.players[event.player_index]
@@ -25,16 +24,15 @@ script.on_event(defines.events.on_player_created, function(event)
 
     inventory.sort_and_merge()
 
-    if not global.init_ran then
-        global.init_ran = true
+    if not storage.init_ran then
+        storage.init_ran = true
 
         if remote.interfaces.freeplay then
             local player = game.players[event.player_index]
             local surface = player.surface
             local sps = surface.find_entities_filtered{position = player.force.get_spawn_position(surface), radius = 250, name = "crash-site-spaceship"} 
-            local position = sps[1].insert({name="assembling-machine-1",count=10})
+            local position = sps[1].insert({name="assembling-machine-1",count=20})
             local position = sps[1].insert({name="burner-inserter",count=10})
-            local position = sps[1].insert({name="small-electric-pole",count=100})
         end
     end
 
@@ -44,15 +42,13 @@ script.on_event(defines.events.on_player_created, function(event)
     end
 end)
 
-script.on_event(defines.events.on_player_respawned, function(event)
-	local player = game.get_player(event.player_index)
-	util.insert_safe(player, {["firearm-magazine"] = 40})
-end)
-
 script.on_init(function()
     if remote.interfaces.freeplay then
         remote.call("freeplay", "set_disable_crashsite", false)
     end
+
+    game.difficulty_settings.spoil_time_modifier = 10
+    game.difficulty_settings.technology_price_multiplier = 10
 end)
 
 
